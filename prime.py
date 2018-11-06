8b5032143bf035d8fbe8fbb71498a3ac8be358b5
Bernease Herman <bernease@gmail.com>
2018-11-06 14:43:02 -0800

    Changed prompt in prime

--- prime.py
+++ prime.py
@@ -20,7 +20,7 @@ def check_prime(num):
 
 # Run the following code if the file is run at the command line
 if __name__ == "__main__":
-  num = int(input("Enter a number: "))
+  num = int(input("Please provide a number to check: "))
   if check_prime(num):
     print ("Is prime!")
   else:
